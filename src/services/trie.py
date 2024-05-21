3a0cda684e32e9f5fc1b245552bad84fa885c37c
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-05-21 08:37:02 +0300

    edit trie to dict

--- src/services/trie.py
+++ src/services/trie.py
@@ -1,12 +1,9 @@
-import pygtrie
-
 class Trie:
     def __init__(self):
-        self.trie = pygtrie.CharTrie()
+        self.words = {}
     
     def insert(self, word):
-        self.trie[word] = True
+        self.words[word] = True
 
     def keys(self):
-        return self.trie.keys()
-
+        return self.words.keys()
