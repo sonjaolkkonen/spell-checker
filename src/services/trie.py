27d0033e45d2b6f86edd9794dee0afa4ae930e94
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-05-29 12:22:20 +0300

    first version of trie and insert, search, get_words, _dfs methods

--- src/services/trie.py
+++ src/services/trie.py
@@ -1,34 +1,77 @@
+class TrieNode:
+    def __init__(self):
+        """class constructor which describes a single node of the trie
+        """
+        self.children = {}
+        self.end_of_word = False
+
 class Trie:
     def __init__(self):
-        """Class constructor which initializes a new Trie object (now it's a an empty dictionary)
+        """Class constructor which initializes a new Trie object
         """
-        self.words = {}
+        self.root = TrieNode()
     
     def insert(self, word):
-        """Inserts words into the trie (currently dictionary).
+        """Inserts words into the trie
 
         Args:
-            word (str): The word to be inserted.
+            word (str): the word to be inserted
         """
-        self.words[word] = True
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                 node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end_of_word = True
 
-    def keys(self):
-        """Returns all the words stored in the trie (currentyly dictionary).
+    def search(self, word):
+        """Search for a given word from the trie
+
+        Args:
+            word (str): The word to be searched
 
         Returns:
-            dict_keys: An iterable view of the dictionary's keys.
+            bool: True if the word is found, otherwise False
         """
-        return self.words.keys()
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end_of_word
+    
+    def get_words(self):
+        """Get all the words stored in the trie
+
+        Returns:
+            list: list of words stored in the trie
+        """
+        words = []
+        self._dfs(self.root, "", words)
+        return words
+    
+    def _dfs(self, node, prefix, words):
+        """Depth-first search to collect all words from the trie
+
+        Args:
+            node (TrieNode): the current node
+            prefix (str): the current prefix of the word
+            words (list): the list to collect words
+        """
+        if node.end_of_word:
+            words.append(prefix)
+        for char, next_node in node.children.items():
+            self._dfs(next_node, prefix + char, words)
 
     @staticmethod
     def load_words(file_path):
-        """Loads words from a file and returns them as a list.
+        """Loads words from a file and returns them as a list
 
         Args:
-            file_path (str): Path to the file containing words.
+            file_path (str): Path to the file containing words
 
         Returns:
-            list: a list of words.
+            list: a list of words
         """
         with open(file_path, 'r') as file:
             return [line.strip() for line in file.readlines()]
