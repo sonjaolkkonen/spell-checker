1e57a13bca8f224c3391b671bce9106bbfd68f1d
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-05-30 08:49:36 +0300

    add unittests for get_words_with_prefix method

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -27,3 +27,17 @@ class TestTrie(unittest.TestCase):
         words = self.trie.get_words()
         expected_words = ["kissa", "koira", "lehmä", "apina", "hevonen"]
         self.assertCountEqual(words, expected_words)
+    
+    def test_get_words_with_prefix(self):
+        self.assertEqual(self.trie.get_words_with_prefix("kis"), ["kissa"])
+        self.assertEqual(self.trie.get_words_with_prefix("k"), ["kissa", "koira"])
+        self.assertEqual(self.trie.get_words_with_prefix("lehm"), ["lehmä"])
+        self.assertEqual(self.trie.get_words_with_prefix("ap"), ["apina"])
+        self.assertEqual(self.trie.get_words_with_prefix("hevo"), ["hevonen"])
+    
+    def test_get_words_with_empty_prefix(self):
+        self.assertEqual(self.trie.get_words_with_prefix(""), ["kissa", "koira", "lehmä", "apina", "hevonen"])
+
+    def test_get_words_with_prefix_does_not_exist(self):
+        self.assertEqual(self.trie.get_words_with_prefix("karhu"), [])
+        
