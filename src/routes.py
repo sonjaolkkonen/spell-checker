848b69c26556acba0887e6b9b48c7d3f07a67d10
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-06-06 13:47:56 +0300

    add check_spelling method

--- src/routes.py
+++ src/routes.py
@@ -4,15 +4,22 @@ from services.spell_checker import SpellChecker
 app = Flask(__name__)
 spell_checker = SpellChecker()
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
     """Handles the index page and processes the form submission.
 
     Returns:
         index.html page
     """
-    suggestions = None
-    if request.method == "POST":
-        text = request.form["text"]
-        suggestions = spell_checker.suggest_text(text)
+    return render_template("index.html")
+
+@app.route("/check_spelling", methods=["POST"])
+def check_spelling():
+    word = request.form.get("input_word")
+    if spell_checker.find_word(word):
+        suggestions = "Ei kirjoitusvirheitä"
+        return render_template("index.html", suggestions=suggestions)
+    suggestions = spell_checker.suggest(word)
     return render_template("index.html", suggestions=suggestions)
+    
+
