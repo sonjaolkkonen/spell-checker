ba0c7fb576991ab45da607e86b874ae27a711da3
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-06-07 12:38:37 +0300

    edit and add unittests for spellchecker

--- src/tests/spell_checker_test.py
+++ src/tests/spell_checker_test.py
@@ -8,27 +8,52 @@ class TestSpellChecker(unittest.TestCase):
         self.spell_checker = SpellChecker()
         self.spell_checker.trie = Trie()
         self.spell_checker.dl = DamerauLevenshtein()
-        words = ["omena", "banaani", "appelsiini"]
+        words = ["apple", "app", "appreciate", "book", "bad", "bear", "bat"]
         for word in words:
             self.spell_checker.trie.insert(word)
 
-    def test_no_typo(self):
-        self.assertEqual(self.spell_checker.suggest("omena"), "Ei kirjoitusvirheitä")
+    def test_suggest_with_no_typo(self):
+        self.assertEqual(self.spell_checker.suggest("apple"), "Ei kirjoitusvirheitä")
 
-    def test_empty_input(self):
+    def test_suggest_with_empty_input(self):
         self.assertEqual(self.spell_checker.suggest(""), "Anna sana")
+    
+    def test_suggest_with_numeric_input(self):
+        self.assertFalse(self.spell_checker.suggest("123"))
 
-    def test_single_typo(self):
-        self.assertIn("omena", self.spell_checker.suggest("omen"))
+    def test_suggest_single_typo(self):
+        self.assertIn("appreciate", self.spell_checker.suggest("apreciate"))
 
-    def test_insertion(self):
-        self.assertIn("appelsiini", self.spell_checker.suggest("apelsiini"))
+    def test_suggest_with_word_not_in_trie(self):
+        self.assertEqual(self.spell_checker.suggest("application"), [])
 
-    def test_deletion(self):
-        self.assertIn("banaani", self.spell_checker.suggest("baanaani"))
+    def test_find_word_with_word_in_trie(self):
+        self.assertTrue(self.spell_checker.find_word("app"))
 
-    def test_transposition(self):
-        self.assertIn("omena", self.spell_checker.suggest("omean"))
+    def test_find_word_with_word_not_in_trie(self):
+        self.assertFalse(self.spell_checker.find_word("battery"))
 
-    def test_no_suggestions(self):
-        self.assertEqual(self.spell_checker.suggest("kakku"), [])
+    def test_find_word_with_numeric(self):
+        self.assertFalse(self.spell_checker.find_word("123"))
+
+    def test_fix_typos_no_typo(self):
+        self.assertEqual(self.spell_checker.fix_typos(["apple", "book", "bat"]), ["apple", "book", "bat"])
+
+    def test_fix_typos_with_typos(self):
+        self.assertEqual(self.spell_checker.fix_typos(["applle", "boot", "apreciate"]), ["apple", "book", "appreciate"])
+
+    def test_fix_typos_with_empty_list(self):
+        self.assertEqual(self.spell_checker.fix_typos([]), [])
+    
+    def test_parse_text(self):
+        self.assertEqual(self.spell_checker.parse_text("apple book appreciate"), ["apple", "book", "appreciate"])
+
+    def test_parse_empty_string(self):
+        self.assertEqual(self.spell_checker.parse_text(""), [])
+
+    def test_return_into_text(self):
+        self.assertEqual(self.spell_checker.return_into_text(["apple", "book", "appreciate"]), "apple book appreciate")
+
+    def test_return_into_text_empty_list(self):
+        self.assertEqual(self.spell_checker.return_into_text([]), "")
+
