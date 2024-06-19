d569b936bd8ed793cd764dfaf2d6ea9b1abd2746
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2024-06-19 23:03:21 +0300

    edit unittests

--- src/tests/damerau_levenshtein_test.py
+++ src/tests/damerau_levenshtein_test.py
@@ -4,26 +4,21 @@ from services.damerau_levenshtein import DamerauLevenshtein
 class TestDamerauLevenshtein(unittest.TestCase):
     def setUp(self):
         self.dl = DamerauLevenshtein()
+        self.word1 = "aalto"
+        self.word2 = "aaltoallas"
 
     def test_same_words(self):
-        word = "kissa"
-        distance = self.dl.damerau_levenshtein_distance(word, word)
+        distance = self.dl.damerau_levenshtein_distance(self.word1, self.word1)
         self.assertEqual(distance, 0)
 
     def test_empty_strings(self):
-        word1 = ""
-        word2 = ""
-        distance = self.dl.damerau_levenshtein_distance(word1, word2)
+        distance = self.dl.damerau_levenshtein_distance("", "")
         self.assertEqual(distance, 0)
 
     def test_different_words(self):
-        word1 = "kissa"
-        word2 = "koira"
-        distance = self.dl.damerau_levenshtein_distance(word1, word2)
-        self.assertEqual(distance, 3)
+        distance = self.dl.damerau_levenshtein_distance(self.word1, self.word2)
+        self.assertEqual(distance, 5)
 
     def test_one_empty_string(self):
-        word1 = "kissa"
-        word2 = ""
-        distance = self.dl.damerau_levenshtein_distance(word1, word2)
-        self.assertEqual(distance, len(word1))
+        distance = self.dl.damerau_levenshtein_distance(self.word2, "")
+        self.assertEqual(distance, len(self.word2))
